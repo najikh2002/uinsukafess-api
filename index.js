1cf1fcf467eea6510c65fc9155844b9bc10a07a6
“najikh2002” <hizbullahnajihan@gmail.com>
2023-12-05 07:40:30 +0700

    test lagi

--- index.js
+++ index.js
@@ -2,47 +2,55 @@
 require('dotenv').config();
 
 const express = require('express');
-const cors = require('cors');
 const axios = require('axios');
+const cors = require('cors');
 
 
 const PORT = process.env.PORT || 8000;
 const app = express();
 
-app.use(cors()); 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 
 app.get('/', (req, res) => {
-
     return res.status(200).json({message: "Selamat datang di tweet-post-api by Hizbullah Najihan"});
 });
 
 app.post('/createpost', async (req, res) => {
-  const tweet = req.body.text; 
+  const tweet = await req.body.text; 
+  console.log(tweet);
   const response = await createPost(tweet);
-  res.json(response.data);
+  res.json(response);
 });
 
-const createPost = async (tweet) => {
-    const data = JSON.stringify({
+const createPost = (tweet) => {
+    let data = JSON.stringify({
     "text": `${tweet}`
-  });
+    });
 
-  const config = {
+    let config = {
     method: 'post',
     maxBodyLength: Infinity,
     url: 'https://api.twitter.com/2/tweets',
     headers: { 
         'Content-Type': 'application/json', 
-        'Authorization': 'OAuth oauth_consumer_key="Ay0dvrzdqdIBTAX02PGetpvyZ",oauth_token="1730713826360987648-qP3FN0gNXlVxQZi9OQgAQ6gD928JE0",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1701603997",oauth_nonce="88lAeWDVnxb",oauth_version="1.0",oauth_signature="pUtg1s2qEB0%2BLggi%2FuaaGkGMpoU%3D"', 
+        'Authorization': 'OAuth oauth_consumer_key="Ay0dvrzdqdIBTAX02PGetpvyZ",oauth_token="1730713826360987648-qP3FN0gNXlVxQZi9OQgAQ6gD928JE0",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1701732917",oauth_nonce="p1cf2O8HBn5",oauth_version="1.0",oauth_signature="2ihWcksGygt41yNN57NWcc5OOgk%3D"', 
         'Cookie': 'guest_id=v1%3A170084276834714652; guest_id_ads=v1%3A170084276834714652; guest_id_marketing=v1%3A170084276834714652; personalization_id="v1_LdPRUHpKtFK6/BMK0+NYkw=="'
-      },
-    data: data
-  };
+    },
+    data : data
+    };
 
-  return axios.request(config);
+    axios.request(config)
+    .then((response) => {
+    console.log(JSON.stringify(response.data));
+    })
+    .catch((error) => {
+    console.log(error);
+    });
+
 };
+  
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
